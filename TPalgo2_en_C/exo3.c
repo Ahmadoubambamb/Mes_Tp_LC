34574156fd15811bdfcfe0791c75a66a8525cb11
Ahmadoubambamb <121469769+Ahmadoubambamb@users.noreply.github.com>
2024-10-28 21:43:16 +0000

    Update TPalgo2_en_C

---
+++ TPalgo2_en_C/exo3.c
@@ -0,0 +1,24 @@
+#include <studio.h>
+#include <stdlib.h>
+
+void nombre_premier(int n, int *vrai)
+{ int i,j=0;
+   for(i=1;i<=n/2;i++)
+     if(n%i==0)
+         j++;
+     if(j>2)
+      *vrai=1;
+    else 
+      *vrai=0;
+}
+
+int main()
+{ int n , true;
+   nombre_premier(n,&vrai);
+   if(vrai)
+  printf("%d est un nombre premier \n",n);
+   else
+  printf("%d n'est pas un nombre premier",n);
+ return 0;
+}
+     
